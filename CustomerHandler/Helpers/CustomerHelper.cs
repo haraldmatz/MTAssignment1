07859d1eaa1a704717b9a5190f1d4b7b0744ac86
haraldmatz <harald.matz@evry.com>
2014-11-22 10:44:35 +0100

    Green again for getting Haralds Email

---
+++ CustomerHandler/Helpers/CustomerHelper.cs
@@ -0,0 +1,22 @@
+﻿using CustomerHandler.Classes;
+
+namespace CustomerHandler.Helpers
+{
+    public static class CustomerHelper
+    {
+        public static Customer AddHarald()
+        {
+            var customer = new Customer();
+
+            customer.FirstName = "Harald";
+            customer.LastName = "Matz";
+            customer.Address = "Mjölnerbacken 34";
+            customer.City = "Sundbyberg";
+            customer.Zip = "174 48";
+            customer.Telephone = "+46 76 643 41 20";
+            customer.Email = "harald.matz@evry.com";  
+
+            return customer;
+        }
+    }
+}
